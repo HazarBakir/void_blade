8bc33f8decd239e1e0048f0f6f0ad0fb11c3db89
Hazar <hazarbakir21@gmail.com>
2025-05-19 00:01:56 +0300

    feat: create enemy scene

--- Scripts/Player/player_movement.gd
+++ Scripts/Player/player_movement.gd
@@ -1,15 +1,17 @@
 extends CharacterBody2D
-
 var speed = 200
 var mouse_position = null
  
 func _physics_process(delta):
-	
 	velocity = Vector2(0, 0)
 	mouse_position = get_global_mouse_position()
  
 	var direction = (mouse_position - position).normalized()
 	velocity = (direction * speed)
+	
+	#Camera offset
+	$Camera2D.offset.x = (mouse_position.x - global_position.x) / (1920 / 2.0) * 140
+	$Camera2D.offset.y = (mouse_position.y - global_position.y) / (1080 / 2.0) * 140
 	
 	move_and_slide()
 	look_at(mouse_position)
