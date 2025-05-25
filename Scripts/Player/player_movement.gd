797d372f9ff87ba94a39410ae85fb10cb9d7fdb6
Hazar <hazarbakir21@gmail.com>
2025-05-25 23:16:38 +0300

    feat: movement lerping

--- Scripts/Player/player_movement.gd
+++ Scripts/Player/player_movement.gd
@@ -1,23 +1,35 @@
 extends CharacterBody2D
+
 var speed = 600
 var mouse_position = null
+var accel = 10.0 
+var decel = 5.0 
 
 func _ready() -> void:
 	add_to_group("player")
 
 func _physics_process(delta):
 	mouse_position = get_global_mouse_position()
+	
 	if Input.is_action_pressed("move_mouse"):
-		move()
+		move(delta)
+	else:
+		velocity.x = lerp(velocity.x, 0.0, decel * delta)
+		velocity.y = lerp(velocity.y, 0.0, decel * delta)
+	
+	velocity = velocity.limit_length(speed)
+	move_and_slide()
+	
 	camera_offset()
 	look_at(mouse_position)
-
 
 func camera_offset():
 	$Camera2D.offset.x = (mouse_position.x - global_position.x) / (1920 / 2.0) * 140
 	$Camera2D.offset.y = (mouse_position.y - global_position.y) / (1080 / 2.0) * 140
 
-func move():
+func move(delta):
 	var direction = (mouse_position - position).normalized()
-	velocity = (direction * speed)
-	move_and_slide()
+	var target_velocity = direction * speed
+	
+	velocity.x = lerp(velocity.x, target_velocity.x, accel * delta)
+	velocity.y = lerp(velocity.y, target_velocity.y, accel * delta)
