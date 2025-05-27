da5e21fca2f7336eaf889239a856abeba0ddeb88
Hazar <hazarbakir21@gmail.com>
2025-05-27 22:10:39 +0300

    feat(bullet): queue_free aftert stop following

--- Scripts/enemy/bullet.gd
+++ Scripts/enemy/bullet.gd
@@ -5,18 +5,37 @@ var target: CharacterBody2D
 var direction = Vector2.ZERO
 var follow_target = true
 
+func _ready():
+	var follow_timer = Timer.new()
+	follow_timer.wait_time = 1.0
+	follow_timer.one_shot = true
+	follow_timer.timeout.connect(_stop_following)
+	add_child(follow_timer)
+	follow_timer.start()
+
 func _physics_process(delta):
 	move_to_player()
 	position += direction * speed * delta
-	look_at(position + direction)
+	if direction != Vector2.ZERO:
+		look_at(position + direction)
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
 		queue_free()
 
-func _on_timer_timeout() -> void:
+func _stop_following():
 	follow_target = false
 	
+	var destroy_timer = Timer.new()
+	destroy_timer.wait_time = 2.0
+	destroy_timer.one_shot = true
+	destroy_timer.timeout.connect(_destroy_projectile)
+	add_child(destroy_timer)
+	destroy_timer.start()
+
+func _destroy_projectile():
+	queue_free()
+
 func move_to_player():
 	if follow_target and target:
 		direction = (target.position - position).normalized()
