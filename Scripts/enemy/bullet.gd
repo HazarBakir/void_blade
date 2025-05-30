ba04a63d9475ff89bda01c3a94c691a39a04d8b7
Hazar <hazarbakir21@gmail.com>
2025-05-30 11:21:56 +0300

    dude, bullets are fast!

--- Scripts/enemy/bullet.gd
+++ Scripts/enemy/bullet.gd
@@ -1,10 +1,9 @@
 extends Area2D
 
-var speed = 500
+var speed = 300.0
 var target: CharacterBody2D
 var direction = Vector2.ZERO
 var follow_target = true
-
 func _ready():
 	var follow_timer = Timer.new()
 	follow_timer.wait_time = 1.0
@@ -17,8 +16,6 @@ func _ready():
 func _physics_process(delta):
 	move_to_player()
 	position += direction * speed * delta
-	if follow_target and target:
-		look_at(target.position)
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
@@ -40,3 +37,4 @@ func _destroy_projectile():
 func move_to_player():
 	if follow_target and target:
 		direction = (target.position - position).normalized()
+		look_at(target.position)
