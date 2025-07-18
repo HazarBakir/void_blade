ed4d47f2c0d324eab904601eda387996632339a0
Hazar <hazarbakir21@gmail.com>
2025-07-18 15:30:09 +0300

    bugfix: call function increment_kill_count

--- Scripts/enemy/enemy.gd
+++ Scripts/enemy/enemy.gd
@@ -133,7 +133,7 @@ func _apply_shoot_cooldown() -> void:
 	can_shoot = true
 
 func on_death() -> void:
-		target.kill_count += 1
+		target.increment_kill_count()
 		_setup_death_effects()
 		queue_free()
 
