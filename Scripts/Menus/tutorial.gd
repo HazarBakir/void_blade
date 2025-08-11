154b0915438a566d37bbc463e0432357656b49e7
Hazar <hazarbakir21@gmail.com>
2025-08-12 01:04:47 +0300

    ideally working tutorial scene.

--- Scripts/Menus/tutorial.gd
+++ Scripts/Menus/tutorial.gd
@@ -1,53 +1,35 @@
 extends Control
 class_name Tutorial
-@onready var tutorial_animation_player: AnimationPlayer = $tutorial_animation_player
-
+@onready var tutorial_animation_player: AnimationPlayer = $AnimationPlayer
+@onready var canvas_layer: CanvasLayer = $CanvasLayer
 signal tutorial_completed
-
 var tutorial_passed : bool = false
-@export var time_scale = 1.0
-
-const SAVE_FILE_PATH = "user://tutorial_completed.save"
+var escape_pressed : bool = false
 
 func _ready() -> void:
-	
-	if is_tutorial_already_completed():
-		tutorial_passed = true
-		tutorial_completed.emit()
-		return
-	
 	tutorial_animation_player.play("tutorial")
-	
 	add_to_group("tutorial")
 
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("escape"):
+		if escape_pressed == false:
+			escape_pressed = true
+			canvas_layer.layer = 1
+		else:
+			escape_pressed = false
+			canvas_layer.layer = 5
+
 func _process(delta: float) -> void:
-	Engine.time_scale = time_scale
-	if tutorial_passed == false and Input.is_action_pressed("move_mouse"):
+	if tutorial_passed == false and Game.paused == false:
+		if Input.is_action_pressed("move_mouse"):
+			complete_tutorial()
+
+func complete_tutorial():
+	if tutorial_passed == false:
 		tutorial_animation_player.play_backwards("tutorial")
 		tutorial_passed = true
-		
-		save_tutorial_completion()
-		
+		canvas_layer.layer = -1
 		tutorial_completed.emit()
 
 func is_tutorial_passed() -> bool:
 	return tutorial_passed
-
-func is_tutorial_already_completed() -> bool:
-	if FileAccess.file_exists(SAVE_FILE_PATH):
-		var file = FileAccess.open(SAVE_FILE_PATH, FileAccess.READ)
-		if file:
-			var completed = file.get_var()
-			file.close()
-			return completed
-	return false
-
-func save_tutorial_completion() -> void:
-	var file = FileAccess.open(SAVE_FILE_PATH, FileAccess.WRITE)
-	if file:
-		file.store_var(true)
-		file.close()
-
-func reset_tutorial_completion() -> void:
-	if FileAccess.file_exists(SAVE_FILE_PATH):
-		DirAccess.remove_absolute(SAVE_FILE_PATH)
