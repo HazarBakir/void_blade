9790e907d57b40c682a4b7be92b0b7633bf66de5
Hazar <hazarbakir21@gmail.com>
2025-08-19 11:11:01 +0300

    update glow.

--- Scripts/death_zone.gd
+++ Scripts/death_zone.gd
@@ -1,8 +1,6 @@
 extends Area2D
 
 var damage = 20.0
-@onready var enemies_container: Node = get_tree().current_scene.get_node("enemies")
-@onready var player: CharacterBody2D = get_tree().get_first_node_in_group("player")
 
 func _on_area_entered(area: Area2D) -> void:
 	var attack = Attack.new()
